7a61824c4d969ae460f20ccd704773b0e6bd6ac8
ghilbut <ghilbut@gmail.com>
2020-03-31 12:43:50 +0900

    #3 Vue.js 환경 설정

--- vue.js/src/views/Home.vue
+++ vue.js/src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <hello-world />
 </template>
 
 <script lang="ts">
@@ -10,6 +7,7 @@ import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
+  name: 'Home',
   components: {
     HelloWorld
   }
