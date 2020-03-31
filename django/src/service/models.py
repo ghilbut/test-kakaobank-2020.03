6562d9956ade3b3c09718be4223adea6f32b273d
ghilbut <ghilbut@gmail.com>
2020-03-31 23:47:38 +0900

    #4 Django에서 오픈 API로 주차장 정보 수집 (#11)
    * Crawling parking lot infomation by OpenAPI
    * Create parking lot model and update table

---
+++ django/src/service/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class ParkingLot(models.Model):
+  code        = models.PositiveIntegerField(primary_key=True)
+  name        = models.CharField(max_length=128, db_index=True)
+  address     = models.CharField(max_length=512, db_index=True)
+  phone_num   = models.CharField(max_length=16,  db_index=True)
+  json_string = models.TextField()
+  crc32       = models.PositiveIntegerField()
+  version     = models.PositiveIntegerField()
+
+  class Meta:
+    db_table = 'spps_parking_lot'
+
+  def __str__(self):
+    return f'[{self.code}] {self.name}'
