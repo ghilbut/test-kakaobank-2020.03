f0ddd0bd5471f22f31af9fcdb98d3608dc8e54d2
ghilbut <ghilbut@gmail.com>
2020-04-01 19:55:11 +0900

    [Vue.js] 주차장 목록을 보여준다.

--- vue.js/src/store.ts
+++ vue.js/src/store.ts
@@ -4,7 +4,31 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    count: 0,
+    prev: null,
+    next: null,
+    list: [],
+    page: 1
+  },
+  mutations: {
+    reset (state, { count, next, previous, results }) {
+      console.log(count, next, previous, results);
+      state.count = count;
+      state.prev = previous;
+      state.next = next;
+      state.list = results;
+    }
+  },
+  actions: {
+    async reset ({ commit }) {
+      try {
+        const uri = `${process.env.VUE_APP_DJANGO_URI}/parking_lots/`;
+        const res = await fetch(uri);
+        commit('reset', await res.json());
+      } catch (e) {
+        alert('Vuex reset failed');
+      }
+    }
+  }
 });
