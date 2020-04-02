a205ab53fb4980a40ec9aa957091e12ce742dd13
ghilbut <ghilbut@gmail.com>
2020-04-02 21:45:49 +0900

    #23 [Django] API 문서를 작성한다. (#40)
    * Apply drf-yasg

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Seoul Public Parking-lot Service
-서울열린데이터 광장의 ‘서울시 공영주차장 안내 정보'를 활용해서 주차 가능한 주차장을 찾을 수 있는 서비스 구현
+서울열린데이터 광장의 [**서울시 공영주차장 안내 정보**](http://data.seoul.go.kr/dataList/OA-13122/S/1/datasetView.do)를 활용해서 주차 가능한 주차장을 찾을 수 있는 서비스 구현
 
 
-## Local Environment
+
+## A. Local Environment
 
 ```bash
 $ git clone https://github.com/ghilbut/seoul-public-parking-lot-service.git
 ```
 
 
-### Django
+### A1. Django
 
 #### 개발환경
 
@@ -52,19 +53,34 @@ $ pipenv run ./src/manage.py runserver 0:8000
 $ pipenv run ./src/manage.py crawling
 ```
 
-Check on http://localhost:8000  
-Login as superuser on http://localhost:8000/admin
+#### 결과페이지
 
-### Vue.js
-
-
-## Production Environment
-
-
-### Terraform
-
-
-### Django
+- REST API:
+  - http://localhost:8000/parking_lots/
+  - http://localhost:8000/parking_lots/{code:int}/
+- Admin Page: http://localhost:8000/admin
+- Documents:
+  - Swagger UI: http://localhost:8000/swagger/
+  - Swagger Json: http://localhost:8000/swagger.json
+  - Swagger Yaml: http://localhost:8000/swagger.yaml
+  - Redoc: http://localhost:8000/redoc/
 
 
-### Vue.js
+### A2. Vue.js
+
+#### 결과페이지
+
+- http://localhost:8080/
+
+
+
+## B. Production Environment
+
+
+### B1. Terraform
+
+
+### B2. Django
+
+
+### B3. Vue.js
--- django/src/service/rest.py
+++ django/src/service/rest.py
@@ -1,8 +1,21 @@
 import json
 from django.db.models import Q
+from django.utils.decorators import method_decorator
+from drf_yasg.openapi import (
+  IN_PATH,
+  IN_QUERY,
+  Parameter,
+  TYPE_INTEGER,
+  TYPE_STRING,
+)
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.routers import DefaultRouter
-from rest_framework.serializers import Serializer
+from rest_framework.serializers import (
+  CharField,
+  Serializer,
+)
+from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import ParkingLot
 
@@ -12,12 +25,53 @@ class ParkingLotSerializer(Serializer):
     return json.loads(value.json_string)
 
 
+class ErrorSerializer(Serializer):
+  detail = CharField()
+
+
 class ParkingLotPagination(PageNumberPagination):
   page_size = 20
+  page_query_description = '조회하려는 페이지의 숫자'
   page_size_query_param = 'size'
+  page_size_query_description = '하나의 페이지에 들어가는 공용주차장의 갯수'
   max_page_size = 1000
 
 
+@method_decorator(
+  name = 'list',
+  decorator = swagger_auto_schema(
+    manual_parameters = [
+      Parameter(
+        'q', IN_QUERY,
+        description = '검색을 위한 키워드 (주차장 이름과 주소, 전화번호가 대상이다)',
+        required = False,
+        type = TYPE_STRING,
+      ),
+    ],
+    operation_summary = '서울시 공용주차장들의 목록을 검색한다.',
+    responses = {
+      HTTP_404_NOT_FOUND: ErrorSerializer('Invalid Page.'),
+    },
+  )
+)
+@method_decorator(
+  name = 'retrieve',
+  decorator = swagger_auto_schema(
+    manual_parameters = [
+      Parameter(
+        'code', IN_PATH,
+        description = '조회하려는 공용주차장의 코드',
+        required = True,
+        type = TYPE_INTEGER,
+      ),
+    ],
+    operation_id = 'parking_lots_retrieve',
+    operation_summary = '선택한 서울시 공용주차장의 상세 정보를 제공한다.',
+    responses = {
+      HTTP_404_NOT_FOUND: ErrorSerializer('Not found.'),
+    },
+  )
+)
 class ParkingLotViewSet(ReadOnlyModelViewSet):
   queryset = ParkingLot.objects.all()
   lookup_field = 'code'
