ea810d3556003a487f762e194fbc041ac2fc08d3
ghilbut <ghilbut@gmail.com>
2020-04-02 23:55:50 +0900

    Create CDN environment for Vue.js application

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,28 @@ terraform {
   required_version = "~> 0.12.6"
 
   backend s3 {
-    bucket  = "seoul-public-parking-lot-terraform"
+    bucket  = "seoul-public-parking-lot-service-terraform-state"
     key     = "terraform.tfstate"
 
-    profile = "ghilbut"
+    profile = "spps"
     region  = "ap-northeast-2"
     encrypt = true
+  }
+}
+
+
+provider aws {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+
+locals {
+  web_domain = "${var.srv_name}.${var.domain_name}"
+  api_domain = "${var.srv_name}-api.${var.domain_name}"
+
+  tags = {
+    owner   = "terraform"
+    purpose = "kakaobank hiring test"
   }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable aws_region {
+  type = "string"
+}
+
+variable aws_profile {
+  type = "string"
+}
+
+variable srv_name {
+  type = string
+}
+
+variable domain_name {
+  type = "string"
+}
