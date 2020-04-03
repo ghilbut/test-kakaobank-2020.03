e988bc88d9290af73eea137801733297e2d2d485
ghilbut <ghilbut@gmail.com>
2020-04-03 13:18:57 +0900

    bugfix

--- django/src/spps/urls.py
+++ django/src/spps/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import (
   include,
   path,
@@ -42,7 +43,12 @@ schema_view = get_schema_view(
 )
 
 
+def healthz(request):
+    return HttpResponse("OK")
+
+
 urlpatterns = [
+    path('', healthz),
     path('admin/', admin.site.urls),
     path('parking_lots/', include(router.urls)),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
